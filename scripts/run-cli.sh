b6a8ee9dcaf2951c40dff9f04ab685195bc84298
João Vitor Perfeito <jo058596@intelbras.com.br>
2024-02-20 16:14:25 -0300

    feat: add treatment for build failure

--- scripts/run-cli.sh
+++ scripts/run-cli.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 npx figlet "Starting CLI..."
-npm run build
+
+if npm run build; then
+
 sudo chmod +x ./dist/main.js
 clear
 npx figlet "CLI Tests"
@@ -13,3 +15,10 @@ read -p "Set the test cases: " testCase
 clear
 npx figlet "CLI Tests"
 sudo npx tests-cli run -d $delay -f $frequency -tc "$testCase"
+
+else
+
+  echo "Build Failed"
+  exit
+
+fi
